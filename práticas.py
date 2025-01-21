44973f8cbbad22c7c5319ae751d728942c056788
Daniel Araújo <contatodanielaraujo@hotmail.com>
2025-01-21 13:11:50 -0300

    Add files via upload

---
+++ práticas.py
@@ -0,0 +1,12 @@
+# coding: utf-8 -*-
+times = 40
+campeonatos = 43
+
+if times > campeonatos:
+    print ("tem mais times")
+
+elif campeonatos < times or campeonatos == 43:
+    print("é o menor ou 43 mesmo")
+
+else:
+    print("requer equilibrio entre os mesmo")
